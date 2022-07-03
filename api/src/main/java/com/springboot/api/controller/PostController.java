2dbb67d9e0b3e4e4e6d2b3b45fb8876bccac8e8f
조재철 <rhsnqk@gmail.com>
2022-07-03 16:25:08 +0900

    post api practice

---
+++ api/src/main/java/com/springboot/api/controller/PostController.java
@@ -0,0 +1,35 @@
+package com.springboot.api.controller;
+
+import com.springboot.api.dto.MemberDto;
+import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    public String postExample() {
+        return "Hello Post Api";
+    }
+
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PostMapping(value = "/member2")
+    public String postMemberDto(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+}
